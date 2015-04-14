c09bf831bd68cbb0f196ddf22ae1d9b7d7d7ac0b
Vitaly Egorov <egorov@samsonos.com>
2015-04-14 14:36:01 +0300

    Added compression support

--- www/index.php
+++ www/index.php
@@ -57,61 +57,6 @@ if (file_exists('../../../src/')) {
     }
 }
 
-// TODO: This code below has to be removed when all applications will upgrade
-
-/**
- * @deprecated Old styled main page rendering, must be build on events
- */
-function oldMainRenderer(&$html)
-{
-    // Render application main page block
-    foreach (\samson\cms\App::loaded() as $app) {
-        // Show only visible apps
-        if ($app->hide == false) {
-            $html .= $app->main();
-        }
-    }
-}
-
-/**
- * @deprecated All application should draw menu block via events
- */
-function oldMenuRenderer(&$html, &$subMenu)
-{
-    $html = '';
-
-    // Iterate loaded samson\cms\application
-    foreach (\samson\cms\App::loaded() as $app) {
-        // Show only visible apps
-        if ($app->hide == false) {
-            // Render application menu item
-            $html .= m('template')
-                ->view('menu/item')
-                ->active(url()->module == $app->id() ? 'active' : '')
-                ->app($app)
-                ->icon($app->icon)
-                ->name(isset($app->name{0}) ? $app->name : (isset($app->app_name{0}) ? $app->app_name : ''))
-                ->output();
-        }
-    }
-
-    $subMenu = '';
-
-    // Find current SamsonCMS application
-    if (\samson\cms\App::find(url()->module, $app/*@var $app App*/)) {
-        // Render main-menu application sub-menu
-        $subMenu = $app->submenu();
-
-        // If module has sub_menu view - render it
-        if ($app->findView('sub_menu')) {
-            $subMenu .= $app->view('sub_menu')->output();
-        }
-    }
-}
-
-// Old applications main page rendering
-\samsonphp\event\Event::subscribe('template.main.rendered', 'oldMainRenderer');
-// Old applications menu rendering
-\samsonphp\event\Event::subscribe('template.menu.rendered', 'oldMenuRenderer');
+require 'old.php';
 
 s()->start('template');
