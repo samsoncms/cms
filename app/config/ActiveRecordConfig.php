8154ca9984f2002ccbc71e065220af8506a4bafa
Vitaly Egorov <egorov@samsonos.com>
2015-05-14 18:20:54 +0300

    Added travis AR config

--- app/config/ActiveRecordConfig.php
+++ app/config/ActiveRecordConfig.php
@@ -1,15 +1,16 @@
 <?php
-namespace samsoncms;
-
 /**
  * Created by Vitaly Iegorov <egorov@samsonos.com>
  * on 19.08.14 at 16:05
  */
+namespace samsoncms\config;
+
+// If this is single SamsonCMS web-application
 if (!defined('EXTERNAL_CONFIG')) {
     /** Test ActiveRecord configuration for development */
     class ActiveRecordConfig extends \samsonphp\config\Entity
     {
-        public $name = 'samsoncms.local';
+        public $name = 'samsonsos.com';
         public $login = 'samsonos';
         public $pwd = 'AzUzrcVe4LJJre9f';
     }
---
+++ app/config/travis/ActiveRecordConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 19.08.14 at 16:05
+ */
+namespace samsoncms\config\travis;
+
+/** Test ActiveRecord configuration for development */
+class ActiveRecordConfig extends \samsonphp\config\Entity
+{
+    public $name = 'samsoncms.local';
+    public $login = 'samsonos';
+    public $pwd = 'AzUzrcVe4LJJre9f';
+}
+
