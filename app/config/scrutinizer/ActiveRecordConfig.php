089f92cc4de08ff9181e4b3f2e2a4640dd7b239e
Vitaly Iegorov <egorov@samsonos.com>
2015-05-17 16:51:07 +0300

    Added scrutinizer environment variable

---
+++ app/config/scrutinizer/ActiveRecordConfig.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 19.08.14 at 16:05
+ */
+namespace samsoncms\config\scrutinizer;
+
+/** Test ActiveRecord configuration for development */
+class ActiveRecordConfig extends \samsonphp\config\Entity
+{
+    public $name = 'samsoncms';
+    public $login = 'root';
+}
+
