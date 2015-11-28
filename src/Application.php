9e7ba1547229fd053cf8da976e35044d80d78fe6
Kotenko <nick.w2r@gmail.com>
2015-11-28 19:12:47 +0200

    fix

--- src/Application.php
+++ src/Application.php
@@ -14,15 +14,26 @@ class Application extends CompressableExternalModule {
     /** @var string Module identifier */
     protected $id = 'cms';
 
+    protected $isCMS = false;
+
     public function init( array $params = array() ) {
+        Event::subscribe('core.security', array($this, 'updateTemplate'));
         // Call parent initialization
         return parent::init( $params );
+    }
+
+    public function updateTemplate($core, $securityResult) {
+        if ($this->isCMS) {
+            $core->template($this->path().'app/view/index.php', true);
+        }
     }
 
     public function initUrl( & $urlObj, & $urlArgs ) {
         if(($urlArgs[0] == 'cms')&&(isset($urlArgs[1]))) {
             $urlArgs[1] = 'cms-'.$urlArgs[1];
             unset($urlArgs[0]);
+            $urlArgs = array_values($urlArgs);
+            $this->isCMS = true;
         }
     }
 
@@ -31,7 +42,5 @@ class Application extends CompressableExternalModule {
         s()->template('app/view/index.php');
         s()->active(m('template'));
         m('template')->__handler();
-
-
     }
 }
