5e43077fcab45de8e812dc384006ba1429dfc509
Alexander Nazarenko <nazarenko@samsonos.com>
2016-03-24 18:03:48 +0200

    added viewport meta tag

--- app/view/index.php
+++ app/view/index.php
@@ -3,6 +3,7 @@
 
     <head>
         <title><?php echo m('cms')->oldGetTitle(); ?> - SamsonCMS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="icon" type="image/png" href="favicon.png">
     </head>
